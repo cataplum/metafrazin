87905db38b40d4675852781cddebf2f6a67dad95
Zoi Gialitaki <zoi.gialitaki@booking.com>
2017-09-23 16:58:47 +0200

    Adding amazing new logo

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-function init() {
 
+function openMenu(){
   $('.hamburger-menu').on('click', function (){
     $('.menu').addClass('col-5 c-menu c-menu--slide-left is-active');
     $('.menu').removeClass('col-3');
     $('.c-mask').addClass('is-active');
   });
+}
 
+function closeMenu(){
   $('.c-mask').on('click', function () {
     $('.menu').removeClass('c-menu c-menu--slide-left is-active');
     $('.c-mask').removeClass('is-active');
   });
+}
 
+function init(){
+  openMenu();
+  closeMenu();
 }
 
 init();
